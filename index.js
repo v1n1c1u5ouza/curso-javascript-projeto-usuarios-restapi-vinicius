3de018ecee5dab11c06cede7d01bbd1fdf505f87
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-26 12:23:58 -0300

    Add: app.use to call routes/index.js and routes/users.js

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const express = require('express');
+let routesIndex = require('./routes/index');
+let routesUsers = require('./routes/users');
 
 let app = express();
 
-app.get('/', (req, res) => {
+app.use(routesIndex);
+app.use('/users', routesUsers);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Olá</h1>');
-
-});
-
-app.get('/users', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users:[{
-            name: 'Hcode',
-            email: 'contato@hcode.com.br',
-            id: 1
-        }]
-    });
-
-});
-
-app.listen(3000, '127.0.0.1', () => {
+    app.listen(3000, '127.0.0.1', () => {
 
     console.log("servidor rodando!");
 
