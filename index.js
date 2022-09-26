8611030ec45a3b5997c750b5f46bb8c011b2bbff
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-26 11:46:05 -0300

    Using express

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-const http= require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
+app.get('/', (req, res) => {
 
-    switch (req.url) {
-
-        case '/':
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Olá</h1>');
-
-        break;
-
-        case '/users':
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({
-            users:[{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
-                id: 1
-            }]
-        }));
-
-        break;
-
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá</h1>');
 
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json')
+    res.json({
+        users:[{
+            name: 'Hcode',
+            email: 'contato@hcode.com.br',
+            id: 1
+        }]
+    });
+
+});
+
+app.listen(3000, '127.0.0.1', () => {
 
     console.log("servidor rodando!");
 
-}); 
+});
