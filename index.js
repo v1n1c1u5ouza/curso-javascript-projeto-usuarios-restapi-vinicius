7063bd6d3eed0eb81ef893d6252d77800f237050
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-26 10:56:01 -0300

    Create the server

---
+++ index.js
@@ -0,0 +1,16 @@
+const http= require('http');
+
+let server = http.createServer((req, res) => {
+
+    console.log('URL:', req.url);
+    console.log('METHOD:', req.method);
+
+    res.end('OK');
+
+});
+
+server.listen(3000, '127.0.0.1', () => {
+
+    console.log("servidor rodando!");
+
+});
