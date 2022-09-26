8fe86d67d9cf93e0cad04f4bebb4751533c1e940
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-26 12:35:41 -0300

    Add: Modules to work if consing

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-let express = require('express');
-let routes = express.Router();
+module.exports = (app) => {
 
-routes.get('/', (req, res) => {
+    app.get('/', (req, res) => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Olá</h1>');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Olá</h1>');
 
-});
+    });
 
-module.exports = routes;
+};
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,27 @@
-let express = require('express');
-let routes = express.Router();
+module.exports = (app) => {
 
-routes.get('/', (req, res) => {
+    app.get('/users', (req, res) => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users:[{
-            name: 'Hcode',
-            email: 'contato@hcode.com.br',
-            id: 1
-        }]
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users:[{
+                name: 'Hcode',
+                email: 'contato@hcode.com.br',
+                id: 1
+            }]
+        });
+
     });
 
-});
+    app.get('/users/admin', (req, res) => {
 
-routes.get('/admin', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: []
+        });
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: []
     });
 
-});
-
-module.exports = routes;
+};
