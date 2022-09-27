1866d5122d2ba4392c4021b9c3f77771d082d011
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-27 08:48:40 -0300

    Add: Nedb Package

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = (app) => {
 
     app.get('/users', (req, res) => {
@@ -16,8 +22,19 @@ module.exports = (app) => {
 
     app.post('/users', (req, res) => {
 
-        res.json(req.body);
+        db.insert(req.body, (err, user) => {
 
+            if (err) {
+                console.log(`erro: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                
+                res.status(200).json(user);
+
+            }
+        });    
     });
 
 };
